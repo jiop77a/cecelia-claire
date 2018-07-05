e44343e96d972a782e9457900a5db3e40fa2371e
Alex E Scott <aescott@outlook.com>
2018-07-04 19:46:33 -0700

    initial attempt

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,27 +2,41 @@ import React from 'react';
 import {images} from '../imageIndex.js';
 import HoverPic from './HoverPic.js';
 import '../css/Gallery.css';
+import Masonry from 'react-mason';
 
 
-export default function () {
-  return (
-    <div className='gallery'>
-      <HoverPic bw={images.bw1} c={images.color1}/>
-      <HoverPic bw={images.bw2} c={images.color2}/>
-      <HoverPic bw={images.bw3} c={images.color3}/>
-      <HoverPic bw={images.bw4} c={images.color4}/>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-      <div className="filler"></div>
-    </div>
-  );
+class Gallery extends React.Component {
+
+  state = {width: 0}
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    console.log(window.innerWidth);
+    let width = window.innerWidth;
+    this.setState({width});
+  }
+
+  render() {
+    let {width} = this.state;
+
+    let calcMarg = () => {
+
+    };
+
+    return (
+      <div className='gallery' style={{marginLeft: ''}}>
+        <Masonry>
+          <HoverPic bw={images.bw3} c={images.color3} long={true}/>
+          <HoverPic bw={images.bw1} c={images.color1}/>
+          <HoverPic bw={images.bw2} c={images.color2}/>
+          <HoverPic bw={images.bw4} c={images.color4}/>
+        </Masonry>
+      </div>
+    );
+  }
 }
+
+export default Gallery;
