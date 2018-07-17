8c07efa57e94e3cf047af210f4801a5cd55fc427
Alex E Scott <aescott@outlook.com>
2018-07-16 19:16:07 -0700

    fix order

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,28 +13,9 @@ export default function () {
         ratio={"2-3"}
       />
       <HoverPic
-        bw={images.bw2}
-        c={images.color2}
-        ratio={"2-3"}
-      />
-      <HoverPic
-        bw={images.bw3}
-        c={images.color3}
-        ratio={"3-2"}/>
-      <HoverPic
-        bw={images.bw4}
-        c={images.color4}
-        ratio={"2-3"}
-      />
-      <HoverPic
         bw={images.bw5}
         c={images.color5}
         ratio={"1-1"}
-      />
-      <HoverPic
-        bw={images.bw6}
-        c={images.color6}
-        ratio={"3-4"}
       />
       <HoverPic
         bw={images.bw7}
@@ -42,9 +23,28 @@ export default function () {
         ratio={"3-4"}
       />
       <HoverPic
+        bw={images.bw2}
+        c={images.color2}
+        ratio={"2-3"}
+      />
+      <HoverPic
+        bw={images.bw6}
+        c={images.color6}
+        ratio={"3-4"}
+      />
+      <HoverPic
+        bw={images.bw3}
+        c={images.color3}
+        ratio={"3-2"}/>
+      <HoverPic
         bw={images.bw8}
         c={images.color8}
         ratio={"7-8"}
+      />
+      <HoverPic
+        bw={images.bw4}
+        c={images.color4}
+        ratio={"2-3"}
       />
       <HoverPic
         bw={images.bw9}
